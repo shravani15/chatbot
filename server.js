829def67fca65289657952d38159cc38aa09312b
shravani reddy <shravani0716@gmail.com>
2019-07-25 08:32:28 +0530

    committing initial changes

---
+++ server.js
@@ -0,0 +1,21 @@
+require('dotenv').config({ path: 'variables.env'});
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+
+app.post('/chat', (req,res) => {
+    const { message } = req.body;
+    console.log(message);
+});
+
+app.set('port',process.env.PORT || 5000);
+const server = app.listen(app.get('port'), () => {
+    console.log(`Express running -> PORT ${server.address().port}`);
+})
